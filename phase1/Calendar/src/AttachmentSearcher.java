c51e1a254f1ee13877320f8c2c9e97ea7eaa55a8
xuhaoya3 <ericc.xu@mail.utoronto.ca>
2020-02-29 15:07:08 -0500

    AttachmentSearcher

--- phase1/Calendar/src/AttachmentSearcher.java
+++ phase1/Calendar/src/AttachmentSearcher.java
@@ -3,12 +3,12 @@ package src;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemoSearcher {
+public class AttachmentSearcher {
     //Searches a list of event and return those that are associated with the memo.
-    public List<Event> search(List<Event> events, Memo memo ){
+    public List<Event> search(List<Event> events, Attachment attachment ){
         List<Event> lst = new ArrayList<Event>();
         for (Event event : events) {
-            if (event.getAttachment().contains(memo)){
+            if (event.getAttachment().contains(attachment)){
                 lst.add(event);
             }
         }
