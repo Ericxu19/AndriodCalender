9ad4bb088aac4c8e6331231e658704ee4fe4fec5
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-02-29 17:34:31 -0500

    Changed type signature to return success of creating and deleting users in UserManager and made password go to first line of creating user file

--- phase1/Calendar/src/UserManager.java
+++ phase1/Calendar/src/UserManager.java
@@ -1,22 +1,33 @@
 package src;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class UserManager {
 
     UserManager(){}
     public User createUser(String username, String password) throws IOException {
         User tempUser = new User(username, password);
-        createFile(username);
+        createFile(username, password);
         return tempUser;
     }
-    public void createFile(String username) throws IOException {
+    public boolean createFile(String username, String password) throws IOException {
         File userFile = new File("phase1/Calendar/Users/UserInfo/"+username+".txt");
-        userFile.createNewFile();
+        boolean success = userFile.createNewFile();
+        if(success){
+            OutputStream os = new FileOutputStream(userFile);
+            byte[] pwBytes = password.getBytes();
+            os.write(pwBytes);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    public void deleteUser(String username) throws IOException {
+    public boolean deleteUser(String username) throws IOException {
         File userFile = new File("phase1/Calendar/Users/UserInfo/"+username+".txt");
-        userFile.delete();
+        return userFile.delete();
     }
 }
