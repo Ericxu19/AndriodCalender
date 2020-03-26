7ba51f2cb91a999e78f73fa3a20f19b52d9eca48
benzhang13 <benzhang13@gmail.com>
2020-03-26 15:48:43 -0400

    added gui components

--- phase2/Phase2Calendar/app/src/main/java/com/example/phase2calendar/MainActivity.java
+++ phase2/Phase2Calendar/app/src/main/java/com/example/phase2calendar/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.phase2calendar;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loginScreen(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void registerScreen(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
