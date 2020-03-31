55c837ff13e300a589cbb4121036489adc38a5b4
jwong8448 <jordann.wong@mail.utoronto,ca>
2020-03-30 21:40:48 -0400

    Took in user from Login for main menu.

--- phase2/Phase2Calendar/app/src/main/java/com/example/phase2calendar/MenuActivity.java
+++ phase2/Phase2Calendar/app/src/main/java/com/example/phase2calendar/MenuActivity.java
@@ -2,7 +2,11 @@ package com.example.phase2calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.phase2calendar.logic.User;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //Gets the User object and creates the Calender around it
+        Intent intent = getIntent();
+        User user = (User) intent.getSerializableExtra("currentUser");
+
+        TextView textView = findViewById(R.id.textView6);
+        textView.setText();
+
+
     }
 
     
