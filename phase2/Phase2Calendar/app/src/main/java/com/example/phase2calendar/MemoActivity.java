ca0289aaa4456f2435dbb9e5712ba54fcd956be5
Eric <32289045+Ericxu19@users.noreply.github.com>
2020-03-31 23:02:18 -0700

    create new memo, memo list and look at memo

--- phase2/Phase2Calendar/app/src/main/java/com/example/phase2calendar/MemoActivity.java
+++ phase2/Phase2Calendar/app/src/main/java/com/example/phase2calendar/MemoActivity.java
@@ -2,9 +2,7 @@ package com.example.phase2calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MemoActivity extends AppCompatActivity {
 
@@ -12,11 +10,5 @@ public class MemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo);
-
-        Button memoBack = findViewById(R.id.memoBack);
-        memoBack.setOnClickListener(v -> {
-            Intent memoGoBack = new Intent(getApplicationContext(), MenuActivity.class);
-            startActivity(memoGoBack);
-        });
     }
 }
